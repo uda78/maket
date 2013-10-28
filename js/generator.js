b48ad53d3cdfc861cc37c2bbc6b8a3f33d40eba2
uda78 <uda7885@gmail.com>
2013-10-29 00:57:43 +0400

    Maket tour company

---
+++ js/generator.js
@@ -0,0 +1,23 @@
+var list = document.getElementById('catalog1');
+		var li = document.createElement('p');
+		for(var i=0; i<14; i++) {
+			var y = "Много текста о Египте ";
+			li.innerHTML += y;
+		}
+		list.appendChild(li);
+		
+var list = document.getElementById('catalog2');
+		var li = document.createElement('p');
+		for(var i=0; i<14; i++) {
+			var y = "Много текста о Турции ";
+			li.innerHTML += y;
+		}
+		list.appendChild(li);
+		
+var list = document.getElementById('catalog3');
+		var li = document.createElement('p');
+		for(var i=0; i<14; i++) {
+			var y = "Много текста о Испании ";
+			li.innerHTML += y;
+		}
+		list.appendChild(li);		
